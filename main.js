ec9f201e67922f9baeaf292ca782bf3e45fa1b8e
stevenr22 <rojasguerreros185@gmail.com>
2023-11-29 11:19:07 -0500

    login y registro listo

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
   document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
   });
+
+  
+//MOSTRAR Y OCULTAR CONTRASEÑA
+const passwordInput = document.getElementById('password'); // Cambia 'contra' a 'Ncontra'
+const showPasswordCheckbox = document.getElementById('mostrarContra');
+
+showPasswordCheckbox.addEventListener('change', function () {
+    if (showPasswordCheckbox.checked) {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+});
