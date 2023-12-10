d9050c33ee2f94b5dfb49c7a4ec1ec4fb716eb64
stevenr22 <rojasguerreros185@gmail.com>
2023-12-09 22:10:44 -0500

    listo

--- partes/menu.php
+++ partes/menu.php
@@ -5,7 +5,7 @@
             <span class="hide-menu">Inicio</span>
         </li>
         <li class="sidebar-item">
-            <a class="sidebar-link" href="../módulos_admin/dashboard.php" aria-expanded="false">
+            <a class="sidebar-link" href="../modulos_admin/dashboard.php" aria-expanded="false">
                 <span>
                   <i class="ti ti-layout-dashboard"></i>
                 </span>
