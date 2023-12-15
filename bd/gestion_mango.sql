81411325ed29159ec236c5ff7387cd6147f1eb75
stevenr22 <rojasguerreros185@gmail.com>
2023-12-14 22:03:30 -0500

    oñppñ

--- bd/gestion_mango.sql
+++ bd/gestion_mango.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-12-2023 a las 21:28:52
+-- Servidor: localhost
+-- Tiempo de generación: 15-12-2023 a las 04:03:13
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestion_mango`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `parcela`
+--
+
+CREATE TABLE `parcela` (
+  `id_parcela` int(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `ancho` decimal(10,0) NOT NULL,
+  `alto` decimal(10,0) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `estado` int(255) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -50,11 +65,11 @@ INSERT INTO `rol` (`id_rol`, `cargo`, `descripcion`, `estado`) VALUES
 --
 
 CREATE TABLE `usuario` (
-  `id_usu` int(11) NOT NULL,
+  `id_usu` int(255) NOT NULL,
   `nombre_completo` text NOT NULL,
   `correo` varchar(255) NOT NULL,
   `nombre_usu` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
   `estado` int(11) NOT NULL DEFAULT 1,
   `id_rol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -63,14 +78,21 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usu`, `nombre_completo`, `correo`, `nombre_usu`, `contraseña`, `estado`, `id_rol`) VALUES
-(1, 'Steven Rojas', 'rojas@gmail.com', 'admin', '123', 1, 1),
+INSERT INTO `usuario` (`id_usu`, `nombre_completo`, `correo`, `nombre_usu`, `clave`, `estado`, `id_rol`) VALUES
+(1, 'Steven Galo Rojas', 'rojas185@gmail.com', 'admin', '123', 1, 1),
 (2, 'Armando Rojas', 'armanddo@gmail.com', 'Arojas', '123', 1, 2),
-(3, 'Carmen Guerrero', 'carmen@gmail.com', 'Crojas', '123', 1, 3);
+(3, 'Carmen Ana Guerrero', 'carmen@gmail.com', 'Crojas', '123', 1, 3),
+(4, 'Galo Rojas', 'galo123@gmail.com', 'galo', '123', 1, 3);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `parcela`
+--
+ALTER TABLE `parcela`
+  ADD PRIMARY KEY (`id_parcela`);
 
 --
 -- Indices de la tabla `rol`
@@ -90,6 +112,12 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `parcela`
+--
+ALTER TABLE `parcela`
+  MODIFY `id_parcela` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
@@ -99,7 +127,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usu` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
